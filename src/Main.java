f38d570fadb55a581ef36d4663ef3c48b39bab29
netotest <stepanmokan@ya.ru>
2024-01-28 18:52:13 +0300

    secondCommit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         int ticketPrice = 5_000;
-        int bonus = ticketPrice / 20;
+        int coefficient = 20;
+        int bonus = ticketPrice/coefficient;
         System.out.println("Начислено "+(bonus)+" бонусов");
     }
 }
