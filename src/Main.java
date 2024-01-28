7e882bf4b925d452d8d30833113d81a5295d889b
netotest <stepanmokan@ya.ru>
2024-01-28 17:30:50 +0300

    firstCommit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5_000;
+        int bonus = ticketPrice / 20;
+        System.out.println("Начислено "+(bonus)+" бонусов");
+    }
+}
